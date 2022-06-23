d59e669eb57c29234a36a3e4d151ad09c097151d
xsw <xsw006007@gmail.com>
2022-06-23 10:43:46 +0800

    Use like to delete finish

--- delete.php
+++ delete.php
@@ -1,3 +1,45 @@
+<?php require_once('Connections/MySqlConn.php'); ?>
+<?php
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  $theValue = (!get_magic_quotes_gpc()) ? addslashes($theValue) : $theValue;
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+
+if ((isset($_GET['id'])) && ($_GET['id'] != "")) {
+  $deleteSQL = sprintf("DELETE FROM db2 WHERE ID=%s",
+                       GetSQLValueString($_GET['id'], "int"));
+
+  mysql_select_db($database_MySqlConn, $MySqlConn);
+  $Result1 = mysql_query($deleteSQL, $MySqlConn) or die(mysql_error());
+
+  $deleteGoTo = "index.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $deleteGoTo .= (strpos($deleteGoTo, '?')) ? "&" : "?";
+    $deleteGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $deleteGoTo));
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
