8d014f4ba0756520d79859a87eeabd8d3a5d99a4
xsw <xsw006007@gmail.com>
2022-06-23 09:25:47 +0800

    Create branch name AddPage

--- add.php
+++ add.php
@@ -6,6 +6,20 @@
 </head>
 
 <body>
-add
+<table width="100" border="1" cellspacing="1" cellpadding="1">
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+
 </body>
 </html>
