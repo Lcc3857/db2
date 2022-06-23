6c94b6e3470fc87bfa9960cb35143a764f3abf0f
xsw <xsw006007@gmail.com>
2022-06-23 10:36:46 +0800

    Change table for td name id of from 1 to update

--- detail.php
+++ detail.php
@@ -1,5 +1,53 @@
 <?php require_once('Connections/MySqlConn.php'); ?>
 <?php
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  $theValue = (!get_magic_quotes_gpc()) ? addslashes($theValue) : $theValue;
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "form1")) {
+  $updateSQL = sprintf("UPDATE db2 SET Name=%s, `Old`=%s, Addr=%s WHERE ID=%s",
+                       GetSQLValueString($_POST['Name'], "text"),
+                       GetSQLValueString($_POST['Old'], "int"),
+                       GetSQLValueString($_POST['Addr'], "text"),
+                       GetSQLValueString($_POST['ID'], "int"));
+
+  mysql_select_db($database_MySqlConn, $MySqlConn);
+  $Result1 = mysql_query($updateSQL, $MySqlConn) or die(mysql_error());
+
+  $updateGoTo = "index.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $updateGoTo .= (strpos($updateGoTo, '?')) ? "&" : "?";
+    $updateGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $updateGoTo));
+}
+
 $colname_Recordset1 = "-1";
 if (isset($_GET['id'])) {
   $colname_Recordset1 = (get_magic_quotes_gpc()) ? $_GET['id'] : addslashes($_GET['id']);
@@ -17,7 +65,7 @@ $totalRows_Recordset1 = mysql_num_rows($Recordset1);
 </head>
 
 <body>
-<form name="form1">
+<form method="POST" action="<?php echo $editFormAction; ?>" name="form1">
 <table width="100" border="1" cellspacing="1" cellpadding="1" align="center" style="margin-top:58px">
   <tr>
     <td>ID</td>
@@ -26,16 +74,17 @@ $totalRows_Recordset1 = mysql_num_rows($Recordset1);
     <td>Addr</td>
   </tr>
   <tr>
-    <td><input type="text" value="<?php echo $row_Recordset1['ID']; ?>" readonly="true" /></td>
+    <td><input type="text" value="<?php echo $row_Recordset1['ID']; ?>" name="ID" readonly="true" /></td>
     <td><input type="text" value="<?php echo $row_Recordset1['Name']; ?>" name="Name" /></td>
     <td><input type="text" value="<?php echo $row_Recordset1['Old']; ?>" name="Old" /></td>
     <td><input type="text" value="<?php echo $row_Recordset1['Addr']; ?>" name="Addr" /></td>
   </tr>
   <tr>
-  	<td colspan="2">更新</td>
+  	<td colspan="2"><input type="submit" value="更新" /></td>
 	<td colspan="2" align="right">刪除</td>
   </tr>
 </table>
+<input type="hidden" name="MM_update" value="form1">
 </form>
 </body>
 </html>
